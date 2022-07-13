8f9aefd71bb4a3ffeeb4c163393614349a23a9f5
Castagnet Cyril <45956179+lecascyril@users.noreply.github.com>
2022-07-13 20:12:56 +0200

    Update index.js

--- srcMoralis/index.js
+++ srcMoralis/index.js
@@ -6,8 +6,8 @@ import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <MoralisProvider
-    appId="ev1Ce3kT0fAiF5JHs019rdSNDmNq4hOzWnKfRKWU" 
-    serverUrl= "https://xb64gfjgflth.usemoralis.com:2053/server"
+    appId="" 
+    serverUrl= ""
   >
     <App />
   </MoralisProvider>,
